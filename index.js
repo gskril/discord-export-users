7618c1a5bd3ac59a8bbd742fda314ac094211cef
Greg <gskril@gmail.com>
2021-11-28 17:11:12 -0500

    Refactor

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
 require("dotenv").config()
-const Discord = require("discord.js")
-const client = new Discord.Client({ fetchAllMembers: true })
-const createCsvWriter = require("csv-writer").createObjectCsvWriter
+const { Client } = require("discord.js")
+const ObjectsToCsv = require("objects-to-csv")
+const client = new Client({ fetchAllMembers: true })
 
 client.login(process.env.CLIENT_TOKEN)
-	.catch(err => console.log('Invalid client token'))
+	.catch(err => console.log("Invalid client token"))
+
+client.on("ready", () => {
+	console.log("Discord Members Bot ready")
+})
 
 client.on("message", async (msg) => {
-	if (msg.member && msg.member.hasPermission(['ADMINISTRATOR'])) {
-		if (msg.content === "/members") {
-			let arr = []
-			let guild = msg.guild
-
-			guild.members.cache.forEach((member) => {
-				arr.push({
-					member: member.user.tag,
-					joined: member.joinedAt,
-				})
-			})
-
-			await generateCSV(arr)
-
-			await msg.channel.send(
-				`Here's a list of all ${guild.memberCount} server members!`,
-				{
-					files: ["./members.csv"],
-				}
-			)
+	// Only allow this command from admins
+	if (msg.member && msg.member.hasPermission(["ADMINISTRATOR"])) {
+		// Listen for message that says "/members" (not case sensitive)
+		if (msg.content.toLowerCase() === "$members") {
+			generateCsvOfMembers(msg)
 		}
 	}
 })
 
-async function generateCSV(arr) {
-	const csvWriter = createCsvWriter({
-		path: "members.csv",
-		header: [
-			{ id: "member", title: "Member" },
-			{ id: "joined", title: "Date Joined" },
-		],
+async function generateCsvOfMembers(msg) {
+	let members = []
+	let guild = msg.guild
+
+	guild.members.cache.forEach((member) => {
+		members.push({
+			member: member.user.tag,
+			joined: member.joinedAt,
+		})
 	})
 
-	csvWriter
-		.writeRecords(arr)
-		.then(() => console.log("\nThe CSV file was written successfully with " + arr.length + " members"))
+	const csv = new ObjectsToCsv(members)
+	await csv.toDisk("./members.csv")
+		.then(() => {
+			msg.channel.send(`Here's a list of all ${guild.memberCount} server members!`, { files: ["./members.csv"] })
+		})
+		.catch((err) => {
+			console.log("Error getting a list of Discord guild members.", err)
+			msg.channel.send("Error getting the members of this server.")
+		})
 }
